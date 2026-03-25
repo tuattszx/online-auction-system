794ef1b09643fd527f80d9fcccf560ad9d71fcff
ducBinUnaware <bduc10612@gmail.com>
2026-03-26 02:54:05 +0700

    update database

---
+++ src/main/java/auction/server/dao/DatabaseManager.java
@@ -0,0 +1,25 @@
+package auction.server.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private static final String HOST = "b99ipdmf7qqs8ydf6oel-mysql.services.clever-cloud.com";
+    private static final String DB_NAME = "b99ipdmf7qqs8ydf6oel";
+    private static final String USER = "undbtnmtrkodctzy";
+    private static final String PASS = "kHMJ9qPTCTiPkEuE3Max";
+    private static final String URL = "jdbc:mysql://" + HOST + ":3306/" + DB_NAME;
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Không tìm thấy MySQL Driver!", e);
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
