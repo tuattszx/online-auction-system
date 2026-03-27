88d41f74a66cb168e9955113d13945cfd7c3786a
ducBinUnaware <bduc10612@gmail.com>
2026-03-27 14:54:58 +0700

    fix

---
+++ src/main/java/auction/common/model/users/User.java
@@ -0,0 +1,31 @@
+package auction.common.model.users;
+
+public class User extends Account{
+    private long balance;
+    private String address;
+    public User (){
+        super();
+    }
+    public User(int id,String username,String password,String role,String email,String address,long balance){
+        super(id, username, password, role, email);
+        this.balance=0;
+        this.address=address;
+        this.balance=balance;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+    public void setBalance(long balance) {
+        if (balance >= 0) {
+            this.balance = balance;
+        }
+    }
+}
