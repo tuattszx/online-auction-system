6f1ef69b77e9450840303d0230a4177710cb740b
trangdanghoctin <huyentrangloveher@gmail.com>
2026-04-07 12:18:07 +0700

    updated account view in pro5 view

--- src/main/java/auction/client/controllers/ProfileController.java
+++ src/main/java/auction/client/controllers/ProfileController.java
@@ -1,13 +1,17 @@
 package auction.client.controllers;
 
 import javafx.fxml.FXML;
-
+import javafx.scene.input.MouseEvent;
 public class ProfileController {
     @FXML
     public void onBackToMainClick(javafx.event.ActionEvent event) {
         ViewManager.switchScene(event, "main-view.fxml", "Hệ thống Đấu giá - Trang chủ");
     }
+    @FXML
+    public void OnMouseBacktoMain(MouseEvent event){
+        ViewManager.switchScene(event,"main-view.fxml", "Trang chủ");
 
+    }
     @FXML
     public void onLogoutClick(javafx.event.ActionEvent event) {
         UserSession.loggedInUser = null;
