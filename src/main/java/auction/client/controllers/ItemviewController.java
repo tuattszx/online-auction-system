25148b9188b63a246d77871cdff5952ad2c3561a
trangdanghoctin <huyentrangloveher@gmail.com>
2026-04-07 10:29:17 +0700

    updated uet_logo in item_view

--- src/main/java/auction/client/controllers/ItemviewController.java
+++ src/main/java/auction/client/controllers/ItemviewController.java
@@ -3,9 +3,16 @@ package auction.client.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import javafx.scene.input.MouseEvent;
+
 public class ItemviewController {
     @FXML
     public void onBackToMainClick(ActionEvent event){
         ViewManager.switchScene(event, "main-view.fxml", "Trang chủ");
+    }
+    @FXML
+    public void OnMouseBacktoMain(MouseEvent event){
+        ViewManager.switchScene(event,"main-view.fxml", "Trang chủ");
+
     }
 }
